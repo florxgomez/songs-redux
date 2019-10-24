097e9fa187f50603eee8741d3380974f60fd46cc
Flor <florxgomez@gmail.com>
2019-10-24 12:14:21 -0300

    Updates

---
+++ precache-manifest.67151783d8afe477ee6f49fb1eb4bd55.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a8672eef66d28b1da85b0d1f1422385",
+    "url": "/songs-redux/index.html"
+  },
+  {
+    "revision": "2d8db55409d6536532d0",
+    "url": "/songs-redux/static/js/2.d759c586.chunk.js"
+  },
+  {
+    "revision": "29eae3f7c05a61b38edb",
+    "url": "/songs-redux/static/js/main.eff022f5.chunk.js"
+  },
+  {
+    "revision": "d52b11445c37216b65dd",
+    "url": "/songs-redux/static/js/runtime-main.4a5ad5db.js"
+  }
+]);
